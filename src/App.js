8c760de0ebe3bd425f46d57cd9c1ef4b66d627a6
Allen Timothe <allentimothe@Allens-iMac.home>
2021-04-28 08:28:32 -0400

    Basic React Setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       JASS Supercar Rental
+      </header>
+    </div>
+  );
+}
+
+export default App;
